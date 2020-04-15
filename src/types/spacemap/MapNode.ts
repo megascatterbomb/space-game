ba77a1fedc92b12e89f03759e56178e0b11b883b
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-16 04:05:26 +1200

    moved some functionality from MapNode to SpaceMap

--- src/types/spacemap/MapNode.ts
+++ src/types/spacemap/MapNode.ts
@@ -2,17 +2,19 @@ import { NodeLink } from "./NodeLink";
 import { Faction } from "../interface/baseitem/Faction";
 
 export class MapNode {
-    private id: number;
-    private name: string;
-    private alliance: Faction;
-    private linkedNodes: NodeLink[];
+	private id: number;
+	private name: string;
+	private alliance: Faction;
 
-    constructor(id: number, name: string, alliance: Faction, connections: NodeLink[]) {
-        this.id = id;
-        this.name = name;
-        this.alliance = alliance;
-        this.linkedNodes = connections;
-    }
-
-
+	constructor(id: number, name: string, alliance: Faction) {
+		this.id = id;
+		this.name = name;
+		this.alliance = alliance;
+	}
+	public findName(): string {
+		return this.name;
+	}
+	public findId(): number {
+		return this.id;
+	}
 }
