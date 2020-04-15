4e7c4ef2be5219c49087d8af4adcfed89fdcc051
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-16 04:04:54 +1200

    Added functions to allow for the making of the spacemap

--- src/types/spacemap/SpaceMap.ts
+++ src/types/spacemap/SpaceMap.ts
@@ -1,7 +1,45 @@
 import { MapNode } from "./MapNode";
+import { NodeLink } from "./NodeLink";
+import { Faction } from "../interface/baseitem/Faction";
 
 export class SpaceMap {
-	private nodeMap = new Map();
+	private nodeMap = new Map<MapNode, Set<NodeLink>>();
 
-	constructor(mapNodeArray: MapNode[]) {}
+	private nodesByName = new Map<string, MapNode>();
+	private nodesById = new Map<number, MapNode>();
+
+	constructor(mapNodeArray: MapNode[]) {
+		mapNodeArray.forEach((el) => {
+			this.nodeMap.set(el, new Set<NodeLink>());
+			this.nodesByName.set(el.findName(), el);
+			this.nodesById.set(el.findId(), el);
+		});
+	}
+
+	public addLink(startNode: MapNode, endNode: MapNode, warpStrength: number): void | TypeError {
+		if (this.nodeMap.get(startNode) == undefined || this.nodeMap.get(endNode) == undefined) {
+			throw new TypeError(`${startNode == undefined ? startNode : endNode} is not defined in the SpaceMap.`);
+		}
+		let sLink = new NodeLink(startNode, endNode, warpStrength);
+		let eLink = new NodeLink(endNode, startNode, warpStrength);
+
+		if (this.nodeMap.get(startNode)!.has(sLink) || this.nodeMap.get(startNode)!.has(eLink)) {
+			throw new TypeError(`Edge between ${sLink} and ${eLink} already exists.`);
+		}
+		this.nodeMap.get(startNode)?.add(sLink);
+		this.nodeMap.get(endNode)?.add(eLink);
+	}
+
+	public static GenerateMapNodes(nameList: string[], factionList: Faction[]): MapNode[] | RangeError {
+		if (nameList.length != factionList.length) {
+			throw new RangeError("Argument of incorrect length when generating nodes.");
+		}
+		let a = new Array<MapNode>();
+
+		for (var i = 0; i < nameList.length; i++) {
+			a.push(new MapNode(i + 1, nameList[i], factionList[i]));
+		}
+
+		return a;
+	}
 }
