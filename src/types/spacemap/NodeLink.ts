9ff3f931f72f9d041f9591c39520c146fa40b07f
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-16 18:35:09 +1200

    added to documentation

--- src/types/spacemap/NodeLink.ts
+++ src/types/spacemap/NodeLink.ts
@@ -11,11 +11,16 @@ export class NodeLink {
 		this.endNode = endNode;
 		this.strength = strength;
 	}
-
+    
+    /**
+     * checks if the given warp strength is sufficient to traverse along this link
+     * @param userStrength the warp strength of the user to check
+     */
 	public checkStrength(userStrength: number): boolean | TypeError {
 		if (typeof userStrength == "number") {
 			return userStrength > this.strength;
 		} else {
+            //ERROR HANDLING: checks if input is number
 			throw new TypeError(`Cannot accept type ${typeof userStrength} where number is required.`);
 		}
 	}
--- src/types/spacemap/SpaceMap.ts
+++ src/types/spacemap/SpaceMap.ts
@@ -15,14 +15,22 @@ export class SpaceMap {
 			this.nodesById.set(el.findId(), el);
 		});
 	}
-
+    /**
+     * Adds a link between two nodes
+     * @param startNode the starting node
+     * @param endNode the ending node
+     * @param warpStrength the warp strength requried to go from start to end node.
+     */
 	public addLink(startNode: MapNode, endNode: MapNode, warpStrength: number): void | TypeError {
+
+        //ERROR HANDLING: checking if nodes exist in the map
 		if (this.nodeMap.get(startNode) == undefined || this.nodeMap.get(endNode) == undefined) {
 			throw new TypeError(`${startNode == undefined ? startNode : endNode} is not defined in the SpaceMap.`);
 		}
 		let sLink = new NodeLink(startNode, endNode, warpStrength);
-		let eLink = new NodeLink(endNode, startNode, warpStrength);
-
+        let eLink = new NodeLink(endNode, startNode, warpStrength);
+        
+        //ERROR HANDLING: checking if the link already exists
 		if (this.nodeMap.get(startNode)!.has(sLink) || this.nodeMap.get(startNode)!.has(eLink)) {
 			throw new TypeError(`Edge between ${sLink} and ${eLink} already exists.`);
 		}
@@ -30,7 +38,13 @@ export class SpaceMap {
 		this.nodeMap.get(endNode)?.add(eLink);
 	}
 
+    /**
+     * Generates an array of mapnodes from a list of names and factions
+     * @param nameList list of names for every node
+     * @param factionList list of the faction that each node belongs to
+     */
 	public static GenerateMapNodes(nameList: string[], factionList: Faction[]): MapNode[] | RangeError {
+        //ERROR HANDLING: checking if there is a faction for every node
 		if (nameList.length != factionList.length) {
 			throw new RangeError("Argument of incorrect length when generating nodes.");
 		}
