14f3c37d2ff571067e3384d64381c50fc3e1c53b
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-17 03:21:01 +1200

    added methods to help error checking in other classes

--- src/types/spacemap/NodeLink.ts
+++ src/types/spacemap/NodeLink.ts
@@ -11,17 +11,36 @@ export class NodeLink {
 		this.endNode = endNode;
 		this.strength = strength;
 	}
-    
-    /**
-     * checks if the given warp strength is sufficient to traverse along this link
-     * @param userStrength the warp strength of the user to check
-     */
-	public checkStrength(userStrength: number): boolean | TypeError {
+
+	/**
+	 * checks if the given warp strength is sufficient to traverse along this link
+	 * @param userStrength the warp strength of the user to check
+	 */
+	public boolCheckStrength(userStrength: number): boolean | TypeError {
 		if (typeof userStrength == "number") {
 			return userStrength > this.strength;
 		} else {
-            //ERROR HANDLING: checks if input is number
+			//ERROR HANDLING: checks if input is number
 			throw new TypeError(`Cannot accept type ${typeof userStrength} where number is required.`);
 		}
+	}
+
+	public strengthDifference(userStrength: number): number {
+		return this.strength > userStrength ? this.strength - userStrength : 0;
+	}
+
+	public isEndNode(queryNode: MapNode): boolean {
+		return queryNode == this.endNode;
+	}
+
+	public equals(o: NodeLink): boolean {
+		return o.getEndNode() == this.endNode && o.getStartNode() == this.startNode;
+	}
+
+	public getStartNode() {
+		return this.startNode;
+	}
+	public getEndNode() {
+		return this.endNode;
 	}
 }
